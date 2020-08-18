11f80cd28f4048782822591fe51c0b67d84bfd1a
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-18 12:10:36 +0900

    ユーザー編集機能の導入

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def edit
+  end
+  
+  def update
+    if current_user.update(params_user)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  private
+
+  def params_user
+    params.require(:user).permit(:nickname, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'places/index'
   root "places#index"
+  resources :users, only: [:edit,:update]
 end
