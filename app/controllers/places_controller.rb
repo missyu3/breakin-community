48dd091238e302f26b8cd7224bf8728a33c9da5d
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-18 16:40:41 +0900

    googlemapにマーカーを初期配置しリンクに飛ぶ機能実装

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,12 @@
 class PlacesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'places/index'
   root "places#index"
   resources :users, only: [:edit,:update]
+  resources :places, only: [:index, :new, :create]
 end
