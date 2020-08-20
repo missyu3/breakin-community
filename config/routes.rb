335c064610ee08ff2dc1fdde2b1f4b73a9efc6e6
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-20 11:07:48 +0900

    edit,updateのルーティンング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "places#index"
   resources :users, only: [:edit,:update]
-  resources :places, only: [:index, :new, :create, :show]
+  resources :places, only: [:index, :new, :create, :show, :edit, :update]
 end
