3911f45990817c7f5e07d6b4d80359fcab991324
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-24 19:06:14 +0900

    user_placesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_080557) do
+ActiveRecord::Schema.define(version: 2020_08_24_095412) do
 
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_08_18_080557) do
     t.integer "price", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_user_places_on_place_id"
+    t.index ["user_id"], name: "index_user_places_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +44,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_080557) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_places", "places"
+  add_foreign_key "user_places", "users"
 end
