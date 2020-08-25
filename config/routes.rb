3c2ba144f69b8753e46400421b88bbdfbe9eeff5
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-25 12:22:51 +0900

    ネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "places#index"
   resources :users, only: [:edit,:update]
-  resources :places
-  resources :messages, only: [:new, :create]
+  resources :places do
+    resources :messages, only: [:new, :create]
+  end
 end
