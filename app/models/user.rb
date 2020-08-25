26ac29ea437975041d317b6acb6eaae9cecf6a49
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-25 17:55:11 +0900

    userとplaceの中間テーブルへのアソシエション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          with_options presence: true do
           validates :nickname
          end
-         has_many :places, through: :user_places
          has_many :user_places
+         has_many :places, through: :user_places
          has_many :messages
 end
