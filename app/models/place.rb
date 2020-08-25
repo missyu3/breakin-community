c9784c0408d196618564ef8ef65e7a0376b2e0f8
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-25 12:16:28 +0900

    アソシエーション作成

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,4 +6,5 @@ class Place < ApplicationRecord
   end
   has_many :users, through: :user_places
   has_many :user_places
+  has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          end
          has_many :places, through: :user_places
          has_many :user_places
+         has_many :messages
 end
