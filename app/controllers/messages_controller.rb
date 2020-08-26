3cf9b2188381b5f87d768a77e4b557e68aa6aa0c
mshkhikomaro <mshkhikomaro@gmail.com>
2020-08-26 15:34:48 +0900

    非同期メッセージ投稿機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,24 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     @place = Place.find(params[:place_id])
-    @messages = @place.messages
+    @messages = @place.messages.all
   end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @message = Message.new(params_message)
+    @messages = @place.messages.includes(:user)
+    @user = User.find_by(id: current_user.id)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message, user: @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def params_message
+    params.require(:message).permit(:text).merge(user_id: current_user.id, place_id: params[:place_id])
+  end
+  
 end
