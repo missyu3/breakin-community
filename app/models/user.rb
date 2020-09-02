62d88732537c9eef7688ff5f3aad682d93e5c925
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-02 15:33:40 +0900

    userモデルのテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          has_many :user_places
          has_many :places, through: :user_places
          has_many :messages
+         
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
