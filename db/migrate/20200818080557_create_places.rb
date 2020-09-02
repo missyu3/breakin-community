43c51690167661f9e58d963adcce276f2b66793a
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-02 12:22:20 +0900

    priceカラムの型変更

--- db/migrate/20200818080557_create_places.rb
+++ db/migrate/20200818080557_create_places.rb
@@ -5,7 +5,7 @@ class CreatePlaces < ActiveRecord::Migration[6.0]
       t.string :address,      null: false
       t.time :main_time,      null: false
       t.time :available_time, null: false
-      t.integer :price,       null: false, default: "0"
+      t.integer :price,       null: false
       t.timestamps
     end
   end
