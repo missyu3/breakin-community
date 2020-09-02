53dabd2af2a68f4e93d842ddfa69d675255486c6
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-02 16:06:49 +0900

    messageモデルのテストコード実装

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :place do
     name              {Faker::Address.city}
     address           {Faker::Address.street_address}
-    price              {Faker::Number.number(digits: 10)}
+    price             {Faker::Number.number(digits: 10)}
   end
 end
