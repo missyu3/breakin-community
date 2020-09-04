6a0a09eb45634054d3e0dcee203ab08cc5d9b665
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-04 11:37:17 +0900

    コントローラーをリファクタリングしapplication_controllerに集約

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def check_guest
+    email =resource&.email || params[:user][:email].downcase
+    if email == 'guestlogin@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+    end
+  end
+
   private
 
   def configure_permitted_parameters
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,3 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: [:update, :destroy]
-
-  def check_guest
-    if resource.email == 'guestlogin@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは編集・削除できません。'
-    end
-  end
-
 end
