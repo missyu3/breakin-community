5d645adac4174151f497df032fd3387337681e75
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-10 12:02:35 +0900

    activestorageでuserに画像カラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          has_many :user_places
          has_many :places, through: :user_places
          has_many :messages
+         has_one_attached :image
          
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'}, allow_blank: true
