24a2de426b5aeb0d74195705e6cbe9016e7f76fe
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-18 16:06:59 +0900

    ユーザー削除時にユーザー画像を削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :user_places, dependent: :destroy
   has_many :places, through: :user_places
   has_many :messages, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, allow_blank: true
