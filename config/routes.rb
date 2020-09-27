4ef9cf8840381232a6d08cbd390acddb99332c3b
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-27 15:53:02 +0900

    フォロー今トーラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_064416) do
+ActiveRecord::Schema.define(version: 2020_09_27_064538) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,10 +46,17 @@ ActiveRecord::Schema.define(version: 2020_09_01_064416) do
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
-    t.string "price", null: false
+    t.string "price", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "remark"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "user_places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
