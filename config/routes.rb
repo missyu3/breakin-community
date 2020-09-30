dd11e077f2096713503c1f5acfee9667ced20ae3
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-30 19:17:45 +0900

    routes.rbにコメントアウト追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
   }
+  #deviseのsessionコントロラーに新しくurlを追加
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
   root "places#index"
   resources :users, only: [:edit,:update, :show] do
     resource :relationships, only: [:create, :destroy]
