2ced48156e471656f321c0c6243887a5b95d6683
mshkhikomaro <mshkhikomaro@gmail.com>
2020-09-30 19:18:32 +0900

    factorybotによるリファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 12)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
   end
 end
