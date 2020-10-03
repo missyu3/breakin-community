55ebb2e397b66ac0e4413b0bfdbb8cea93a07d9f
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-03 15:17:52 +0900

    ダンスレベルをストロングパラメータに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :dance_level_id])
   end
 end
