7fb0dee8f099d79774d7bfcfbc9a25fbb467ba2b
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-05 22:48:32 +0900

    factorybotでの練習場所カラム修正

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :place do
-    name              { Faker::Address.city }
-    address           { Faker::Address.street_address }
-    price             { Faker::Number.number(digits: 10) }
+    name { Faker::Address.city }
+    address { Faker::Address.street_address }
+    price { Faker::Lorem.sentence }
+    remark { Faker::Lorem.sentence }
+    url { Faker::Internet.url }
+    lat { Faker::Address.latitude }
+    lng { Faker::Address.longitude }
+    image {Faker::Lorem.sentence}
   end
 end
