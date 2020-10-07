96a344ac86325667a95e7fefb4fffd7f239e74aa
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-07 12:58:19 +0900

    placeコントローラーのテストコードでのエラーを解消

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Places', type: :request do
   before do
     @place = FactoryBot.create(:place)
+    @place.image = fixture_file_upload('app/assets/images/default_place.png')
   end
+
   describe 'GET /places' do
-    # it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
-    #   get places_path
-    #   expect(response).to have_http_status(200)
-    # end
-    # it 'newアクションにリクエストするとログインページへリダイレクトされる' do
-    #   get new_place_path
-    #   expect(response).to have_http_status(302)
-    # end
-    # it 'editアクションにリクエストするとログインページへリダイレクトされる' do
-    #   get edit_place_path(@place.id)
-    #   expect(response).to have_http_status(302)
-    # end
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get places_path
+      expect(response).to have_http_status(200)
+    end
+    it 'newアクションにリクエストするとログインページへリダイレクトされる' do
+      get new_place_path
+      expect(response).to have_http_status(302)
+    end
+    it 'editアクションにリクエストするとログインページへリダイレクトされる' do
+      get edit_place_path(@place.id)
+      expect(response).to have_http_status(302)
+    end
   end
 end
