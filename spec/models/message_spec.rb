dd783241b346deccc86a954d020e6e81661ea461
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-07 12:22:41 +0900

    メッセージモデルのテストコード微修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,21 +6,28 @@ RSpec.describe Message, type: :model do
   end
 
   describe 'メッセージ新規投稿' do
-    it 'textが空だと投稿できない' do
-      @message.text = ''
-      @message.valid?
-      expect(@message.errors.full_messages).to include 'テキストを入力してください'
+    context "メッセージの投稿に成功する場合" do
+      it 'textがあれば投稿に成功する' do
+      expect(@message).to be_valid
+      end
     end
+    context "メッセージの投稿に失敗する場合" do
+      it 'textが空だと投稿できない' do
+        @message.text = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include 'テキストを入力してください'
+      end
 
-    it 'userが紐づいていないと投稿できない' do
-      @message.user = nil
-      @message.valid?
-      expect(@message.errors.full_messages).to include 'Userを入力してください'
-    end
-    it 'placeが紐づいていないと投稿できない' do
-      @message.place = nil
-      @message.valid?
-      expect(@message.errors.full_messages).to include 'Placeを入力してください'
+      it 'userが紐づいていないと投稿できない' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include 'Userを入力してください'
+      end
+      it 'placeが紐づいていないと投稿できない' do
+        @message.place = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include 'Placeを入力してください'
+      end
     end
   end
 end
