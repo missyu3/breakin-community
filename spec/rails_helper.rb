02d7715054caddd795397346bb03b36f5a313778
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-09 23:37:39 +0900

    ドライバーが動くようにする

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,22 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+  
+  config.before(:each, type: :system, js: true) do
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :selenium, using: :chrome, options: {
+        browser: :remote,
+        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+        desired_capabilities: :chrome
+      }
+    else
+      driven_by :selenium_chrome_headless
+    end
+  end
+  
   config.include SignInSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
