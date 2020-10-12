b3c64ca6e8d0d40e4e49128e48407c2022d16fda
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-12 14:45:32 +0900

    webpackerをコンパイルし直す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("./map")
 require("./newmap")
-import 'stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
