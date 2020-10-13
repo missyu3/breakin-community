8bf9f59431f310b5ee1f8851bb85f765d4174e0a
mshkhikomaro <mshkhikomaro@gmail.com>
2020-10-13 12:59:57 +0900

    Update capybara.rb

--- spec/helpers/capybara.rb
+++ spec/helpers/capybara.rb
@@ -13,11 +13,3 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 Capybara.javascript_driver = :selenium_chrome_headless
-
-# chromeというドライバーをcapybaraに登録する
-Capybara.register_driver :chrome do |app|
-	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
-
-	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-Capybara.javascript_driver = :chrome
